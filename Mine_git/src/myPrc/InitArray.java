55e1d97c42cf3721bfd49d160fee69f049f85ce5
rldlfm <rldlfm@qq.com>
2017-04-30 10:01:41 +0800

    ?

--- Mine_git/src/myPrc/InitArray.java
+++ Mine_git/src/myPrc/InitArray.java
@@ -6,7 +6,7 @@ public class InitArray {
 		int[] arr = {1,2,3,4};
 		int[] arr1 = new int[]{2,3,4,5};
 		int[] arr2 = new int[4];
-		arr2[0] = 3;arr2[1] = 3; //..
+		arr2[0] = 3;arr2[1] = 3; //...
 		System.out.println(arr+" " + arr1 + " " + arr2);
 	} 
 }
