1108cdef94f023a5883708e8cec3d2ba900d1c76
lfm <rldlfm@qq.com>
2017-06-11 00:07:33 +0800

    2017/6/11

--- Mine/src/callBack/Main.java
+++ Mine/src/callBack/Main.java
@@ -3,9 +3,10 @@ package callBack;
 public class Main {
 	public static void main(String[] args) {
 		BookBiz bz = new BookBiz();
+		// ....
 		bz.setFilename("books.data");
 		bz.save();
-		System.out.println("main-threadç»§ç»­æ§è¡");
+		System.out.println("main-thread继续执行");
 	}
 
 }
